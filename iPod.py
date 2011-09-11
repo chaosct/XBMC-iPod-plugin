302823640a1866cc6995ff28f9098cc774659803
Carles F. Julià <chaos.ct@gmail.com>
2011-09-11 16:02:59 +0200

    Introducing the new URL system

--- iPod.py
+++ iPod.py
@@ -49,53 +49,91 @@ def MyAddDirectoryItem(url,caption,isFolder=False):
     listItem = xbmcgui.ListItem(caption)
     xbmcplugin.addDirectoryItem(thisPlugin,url,listItem,isFolder=isFolder)
 
+def isUrl(name):
+    def register(f):
+        global urls_views
+        try:
+            urls_views
+        except NameError:
+            urls_views = dict()
+        urls_views[name]=f
+        return f
+    return register
+
+def call_Url(u):
+    global urls_views
+    try:
+        url = u
+        url = url[len(thisPluginUrl):].split('/')
+    except:
+        url = ['']
+    view = url[0]
+    args = url[1:]
+    
+    if not view or view not in urls_views:
+        view = 'list_ipods'
+        args = []
+    
+    args = [base64.b64decode(a) for a in args if a]
+    
+    f = urls_views[view]
+    f(*args)
+
+def make_Url(f,*args):
+    #global urls_views_inverse
+    #try:
+    #    urls_views_inverse
+    #except:
+    urls_views_inverse = dict((v,k) for k, v in urls_views.iteritems())
+    base = urls_views_inverse[f]
+    args = [base64.b64encode(a) for a in args if a]
+    return thisPluginUrl+("/".join([base]+args))
+
+@isUrl('list_ipods')
 def firstLevel():
     "List the founs iPods and copy all the info"
     listipods = [m for m in sys_utils.get_mounts() if os.path.exists(os.path.join(m,'iPod_Control','iTunes','iTunesDB'))]
     for m in listipods:
         copyInfo(m)
-        m64 = base64.b64encode(m)
-        MyAddDirectoryItem(thisPluginUrl+m64,m,isFolder=True)
+        MyAddDirectoryItem(make_Url(ListAllAlbums,m),m,isFolder=True)
     xbmcplugin.endOfDirectory(thisPlugin)
 
-def ListAllAlbums(url):
-    mp = base64.b64decode(url)
+#queries: albums
+
+@isUrl('all_albums')
+def ListAllAlbums(mp):
     d = shelve.open(ipodDB)
     albums = d[mp]
+    ViewListAlbums(mp,sorted(albums))
     d.close()
-    for a in sorted(albums):
+        
+
+def ViewListAlbums(mp,albumlist):
+    for a in albumlist:
         if a:
-            a64 = base64.b64encode(a)
-            MyAddDirectoryItem(thisPluginUrl+('/'.join((url,a64))),a,isFolder=True)
+            MyAddDirectoryItem(make_Url(ListAllSongsFromAlbum,mp,a),a,isFolder=True)
     xbmcplugin.endOfDirectory(thisPlugin)
 
-def ListAllSongsFromAlbum(url):
-    mp = base64.b64decode(url[0])
-    album = base64.b64decode(url[1])
+#queries: songs
+
+@isUrl('songs_from_album')
+def ListAllSongsFromAlbum(mp,album):
     d = shelve.open(ipodDB)
     albums = d[mp]
     d.close()
-    for s in sorted(albums[album]['songs'],key=lambda s1: s1['track number']):
+    ViewListSongs(sorted(albums[album]['songs'],key=lambda s1: s1['track number']))
+
+
+def ViewListSongs(songs):
+    for s in songs:
         MyAddDirectoryItem(s['file'],s['title'])
     xbmcplugin.endOfDirectory(thisPlugin)
 
 def main(a1,a2):
     global thisPlugin
     thisPlugin = int(a2)
-    try:
-        url = a1
-        url = url[len(thisPluginUrl):].split('/')
-    except:
-        url = []
-
-    url = [u for u in url if u]
-
-    if len(url) == 0:
-        firstLevel()
-    elif len(url) == 1:
-        ListAllAlbums(url[0])
-    elif len(url) == 2:
-        ListAllSongsFromAlbum(url)
+    
+    call_Url(a1)
 
 if __name__ == "__main__":
     main(sys.argv[0],sys.argv[1])
